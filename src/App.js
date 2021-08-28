2989e81af85483f9c82d66eb5c588c746ebf64a5
Son Pham <son.pham@nasa.gov>
2021-08-28 11:40:19 -0500

    adding prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, props.name),
-        React.createElement("h3", {}, props.animal),
-        React.createElement("h3", {}, props.breed),
-    ])
-}
-
-
-const App = () => {
-    return React.createElement(
-    "div",
-    {},
-    [ // brackets are optional, but used for readability
-        React.createElement("h1", { id: "my-brand"}, "Hello, World!"),
-        React.createElement(Pet, {name: "Luna", animal:"Dog", breed: "Havanese"}), 
-        React.createElement(Pet, {name: "Pepper", animal:"Bird", breed: "Cockatiel"}), 
-        React.createElement(Pet, {name: "Sudo", animal:"Dog", breed: "Wheaten Terrier"}), 
-    ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
 };
 
+const App = () => {
+  return React.createElement("div", {}, [
+    // brackets are optional, but used for readability
+    React.createElement("h1", { id: "my-brand" }, "Hello, World!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    }),
+  ]);
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
