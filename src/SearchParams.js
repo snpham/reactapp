ea1fe6c26d3bec00a332a916791e29cf66560c8f
Son Pham <son.pham@nasa.gov>
2021-08-29 10:24:39 -0500

    adding options for hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
+    const [animal, setAnimal] = useState("dog");
+    const [breed, setBreed] = useState("");
+    const breeds = [];
 
 
     return (
@@ -15,6 +20,40 @@ const SearchParams = () => {
                         onChange={e => setLocation(e.target.value)} 
                         value={location} 
                         placeholder="Location" />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select 
+                        id="animal"
+                        value={animal}
+                        onChange={e => setAnimal(e.target.value)}
+                        onBlur={e => setAnimal(e.target.value)}>
+                        <option />
+                        {
+                            ANIMALS.map(animal => (
+                                <option value={animal} key={animal} >
+                                    {animal}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select 
+                        id="breed"
+                        value={breed}
+                        onChange={e => setBreed(e.target.value)}
+                        onBlur={e => setBreed(e.target.value)}>
+                        <option />
+                        {
+                            breeds.map((breed) => (
+                                <option value={breed} key={breed} >
+                                    {breed}
+                                </option>
+                            ))
+                        }
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
