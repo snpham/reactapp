5690978905aa32245136392c6f27fc0f6f5840a4
Son Pham <son.pham@nasa.gov>
2021-08-29 09:48:17 -0500

    adding hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 // /* global React ReactDOM */  // use to remove Reach var flags
 import ReactDOM from "react-dom";
-import Pet from "./Pet"
+import SearchParams from "./SearchParams";
 
 
 const App = () => {
   return (
   <div>
     <h1>Hello, World!</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanese" />
-    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-    <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
-    
+    <SearchParams />
   </div>
   )
 }
