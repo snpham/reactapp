25a1a47d45eeb6ccfdf117ef1a31152892664adb
Son Pham <son.pham@nasa.gov>
2021-08-29 09:11:19 -0500

    adding jsx

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 // /* global React ReactDOM */  // use to remove Reach var flags
-import React from 'react';
 import ReactDOM from "react-dom";
 import Pet from "./Pet"
 
 
 const App = () => {
-  return React.createElement("div", {}, [
-    // brackets are optional, but used for readability
-    React.createElement("h1", { id: "my-brand" }, "Hello, World!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
-};
+  return (
+  <div>
+    <h1>Hello, World!</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+    
+  </div>
+  )
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
