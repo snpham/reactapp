b1d390fab327a937b3a4fd9d0724c4ebffa1f681
Son Pham <son.pham@nasa.gov>
2021-09-04 10:26:44 -0500

    adding class components

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,45 @@
-const Details = () => {
-    return <h2>Hello!</h2>
+import {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+
+class Details extends Component {
+    state = {loading: true};
+
+    async componentDidMount () {
+        const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`);
+        const json = await res.json();
+        // this.setState(
+        //     Object.assign(
+        //     {
+        //         loading: false,
+        //     }, 
+        //     json.pets[0])
+        //     )
+        this.setState({
+            loading: false,
+            name: json.pets[0].name,
+            breed: json.pets[0].breed,
+            animal: json.pets[0].animal,
+            city: json.pets[0].city,
+            state: json.pets[0].state,
+            description: json.pets[0].description,
+        
+        })
+    }
+    render () {
+        const {animal, breed, city, state, description, name} = this.state;
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+                    {/* <h2>{animal} - {breed} - {city}, {state}</h2> */}
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
 }
 
 
-
-
-
-export default Details;
+export default withRouter(Details);
